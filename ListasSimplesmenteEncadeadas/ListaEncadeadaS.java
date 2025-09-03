28d2f40da8a7f9a7f20abdc997870afe0fab5bf8
Pedro Cerato <Pedrocerato7717@gmail.com>
2025-09-03 19:20:32 -0300

    Revisão lista simplesmente encadeada - 03/09/2025

---
+++ ListasSimplesmenteEncadeadas/ListaEncadeadaS.java
@@ -0,0 +1,48 @@
+package ListasSimplesmenteEncadeadas;
+
+public class ListaEncadeadaS {
+
+    Nodo inicio;
+
+    private class Nodo {
+        String nome;
+        Nodo proximo;
+    }
+
+    public ListaEncadeadaS() {
+        inicio = null;
+    }
+
+    public void insereInicio(String n) {
+        Nodo novo = new Nodo();
+        novo.nome = n;
+        if (inicio == null) {
+            inicio = novo;
+        } else {
+            novo.proximo = inicio;
+            inicio = novo;
+        }
+    }
+
+    public String removeInicio() {
+        if (inicio == null) {
+            return null;
+        }
+        String removido = inicio.nome;
+        inicio = inicio.proximo;
+
+        return removido;
+    }
+
+    public String removeElemento(String n) {
+        Nodo atual = inicio;
+        Nodo anterior = null;
+        while (atual != null && !atual.nome.equals(n)) {
+            anterior = atual;
+            atual = atual.proximo;
+        }
+    }
+
+
+
+}
